b300bd2fbacea1934ee6a2f662e0bbc20636e188
cassandra <cassiedejucos.03@gmail.com>
2026-09-26 21:25:07 +1000

    deleted all python files, only jupyter nb remaining

--- python_files/demo_error.py
+++
@@ -1,6 +0,0 @@
-from python_files.classes import ParkingViolation, SpeedingViolation, InvalidViolationDataError
-
-try:
-    ParkingViolation("D999", -50, "Paid", 5)
-except InvalidViolationDataError as e:
-    print(f"Rejected as expected: {e}")
